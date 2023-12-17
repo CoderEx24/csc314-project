746b0f530b75b45d0235cefef27bc3d2bf66edb4
CoderEx24 <coderex24@protonmail.com>
2023-12-17 07:49:52 +0200

    frontend: minor refactor

--- src/frontend/loginscreen.py
+++ src/frontend/loginscreen.py
@@ -1,5 +1,5 @@
-from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
 from os import path
 
 Builder.load_file(path.join(path.dirname(__file__), 'kv', 'login.kv'))
